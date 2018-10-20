a48a507ac74943f8ac919994f88c8a0e58adbbd8
freshield <zxdsw199182@gmail.com>
2018-10-20 17:27:29 +0800

    done first httpresponse

---
+++ chp3/MyDjango/index/urls.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: urls.py
+@Time: 2018-10-20 17:21
+@Last_update: 2018-10-20 17:21
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index)
+]
