15d5e48a15854750fd1034c1cc5dd56af27bccb2
freshield <zxdsw199182@gmail.com>
2018-12-23 22:26:44 +0800

    begin chp8

---
+++ chp8/MyDjango/index/urls.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: urls.py
+@Time: 2018-12-23 22:00
+@Last_update: 2018-12-23 22:00
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('insert_data', views.insert_data)
+]
+
---
+++ chp8/MyDjango/index/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+def insert_data(request):
+    Type.objects.create(id=1, type_name='phone')
+    Type.objects.create(id=2, type_name='tablet')
+    Type.objects.create(id=3, type_name='smart_wear')
+    Type.objects.create(id=4, type_name='accessor')
+
+    Product.objects.create(id=1, name='V10', weight='111g', size='157*233*233mm', type_id=1)
+    Product.objects.create(id=2, name='nova', weight='111g', size='157*233*233mm', type_id=1)
+    Product.objects.create(id=3, name='waterplay', weight='111g', size='157*233*233mm', type_id=2)
+    Product.objects.create(id=4, name='changwan', weight='111g', size='157*233*233mm', type_id=2)
+    Product.objects.create(id=5, name='porsche', weight='111g', size='157*233*233mm', type_id=3)
+    Product.objects.create(id=6, name='action_wear', weight='111g', size='157*233*233mm', type_id=3)
+    Product.objects.create(id=7, name='battery', weight='111g', size='157*233*233mm', type_id=4)
+    Product.objects.create(id=8, name='cheng', weight='111g', size='157*233*233mm', type_id=4)
+    Product.objects.create(id=9, name='V10', weight='111g', size='157*233*233mm', type_id=1)
+    Product.objects.create(id=10, name='V10', weight='111g', size='157*233*233mm', type_id=1)
+    Product.objects.create(id=11, name='V10', weight='111g', size='157*233*233mm', type_id=1)
+
+    return HttpResponse('success')
+
