8eca78204694272b611f59201035e0680e9e1346
freshield <zxdsw199182@gmail.com>
2019-01-11 13:52:04 +0800

    begin the chp1

---
+++ chp1/a1_test.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: a1_test.py
+@Time: 2019-01-11 13:49
+@Last_update: 2019-01-11 13:49
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+import django
+
+print(django.__version__)
