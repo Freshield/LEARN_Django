d882d02e72fdd146725d283c3a72e3c99ebae304
freshield <zxdsw199182@gmail.com>
2019-01-17 11:19:23 +0800

    done try the insert data throught the code

--- chp6/MyDjango/index/test_insert_data.py
+++ chp6/MyDjango/index/test_insert_data.py
@@ -16,9 +16,20 @@
 @==============================================@
 """
 from index.models import *
-p = Product()
-p.name = '荣耀V9'
-p.weight = '111g'
-p.size = '120*75*7mm'
-p.type_id = 1
-p.save()
+from django.http import HttpResponse
+
+def test_insert_data(request):
+    # t = Type()
+    # t.id = 4
+    # t.type_name = 'accessory'
+    # t.save()
+
+    p = Product()
+    p.name = 'steelyard'
+    p.weight = '111g'
+    p.size = '120*75*7mm'
+    p.type_id = 4
+    p.save()
+    return HttpResponse('name:%s type:%s'%(p.name, p.type_id))
+# Product.objects.create(
+#     name='荣耀V9', weight='111g', size='120*75*7mm', type_id=1)
---
+++ chp6/MyDjango/index/urls.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: urls.py
+@Time: 2019-01-17 10:47
+@Last_update: 2019-01-17 10:47
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+from django.urls import path
+from . import test_insert_data
+
+urlpatterns = [
+    path('test_insert', test_insert_data.test_insert_data)
+]
